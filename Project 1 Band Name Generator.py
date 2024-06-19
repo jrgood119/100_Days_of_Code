b35d1bb610202cc5104a02fd0751866065add964
JT Good <jrgood119@gmail.com>
2024-06-18 19:28:20 -0500

    Create Project 1 Band Name Generator.py

---
+++ Project 1 Band Name Generator.py
@@ -0,0 +1,8 @@
+# Greeting
+print("Welcome to the Band Name Generator!")
+# Input for city
+city = input("What's the name of the city you grew up in?\n")
+# Input for pet
+pet = input("What's your pet's name?\n")
+# Print message
+print("Your band name could be " + city + " " + pet + "!")
